a9f34b34e7c52f08c9819f46e596c969c095f1d0
millerbm18 <millerbm@smu.edu>
2021-11-08 13:48:33 -0600

    graphml reads in with properties

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,43 @@ using namespace std;
 using namespace boost;
 
 struct GraphData { string Name; };
-struct VertexProperty { string Name; };
+struct VertexProperty {
+    string label;
+    int value;
+    int node_id;
+};
 struct EdgeProperty { string Name; };
 
 using Graph = adjacency_list<setS, vecS, undirectedS, VertexProperty, EdgeProperty, GraphData>;
 
-int main() {
-    dynamic_properties dp(ignore_other_properties);
+Graph ReadGraph(std::string const& fileName) {
     ifstream inFile;
-    inFile.open("/Users/loganschmitt/Documents/21f-pa03-blake-miller-and-logan-schmitt/graph.graphml", std::ifstream::in);
-
-    Graph g;
-
-    try {
-        boost::read_graphml(inFile, g, dp);
-    } catch (boost::exception& xml_parser_error){
-        cout << "error caught : xml_parser_error" << endl;
+    inFile.open(fileName, std::ifstream::in);
+    if(!inFile.is_open()){
+        cout<<"File not open."<<endl;
+        return 0;
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    Graph g;
+    dynamic_properties dp(ignore_other_properties);
+    dp.property("label", boost::get(&VertexProperty::label, g));
+    dp.property("value", boost::get(&VertexProperty::value, g));
+    dp.property("node_id", boost::get(&VertexProperty::node_id, g));
+    boost::read_graphml(inFile, g, dp);
+    return g;
+}
+
+void WriteGraph(std::ostream& os, Graph& graph) {
+    boost::dynamic_properties dp;
+    dp.property("label", boost::get(&VertexProperty::label, graph));
+    dp.property("value", boost::get(&VertexProperty::value, graph));
+    dp.property("node_id", boost::get(&VertexProperty::node_id, graph));
+    boost::write_graphml(os, graph, dp, true);
+}
+
+int main() {
+
+    Graph g = ReadGraph("../graph.graphml");
+    WriteGraph(std::cout << "==== XML version: ====\n\n", g);
     return 0;
 }
