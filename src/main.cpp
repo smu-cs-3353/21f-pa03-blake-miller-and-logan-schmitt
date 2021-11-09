5133c0c9c818b6b4469adb0e73dabad3906bc326
millerbm18 <millerbm@smu.edu>
2021-11-09 10:33:36 -0600

    created GraphAlgos class to hold graph algorithms

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,10 @@
 #include <iostream>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graphml.hpp>
-#include <boost/property_map/dynamic_property_map.hpp>
+#include "GraphAlgos.h"
+
 using namespace std;
-using namespace boost;
-
-struct GraphData { string Name; };
-struct VertexProperty {
-    string label;
-    int value;
-    int node_id;
-};
-struct EdgeProperty { string Name; };
-
-using Graph = adjacency_list<setS, vecS, undirectedS, VertexProperty, EdgeProperty, GraphData>;
-
-Graph ReadGraph(std::string const& fileName) {
-    ifstream inFile;
-    inFile.open(fileName, std::ifstream::in);
-    if(!inFile.is_open()){
-        cout<<"File not open."<<endl;
-        return 0;
-    }
-
-    Graph g;
-    dynamic_properties dp(ignore_other_properties);
-    dp.property("label", boost::get(&VertexProperty::label, g));
-    dp.property("value", boost::get(&VertexProperty::value, g));
-    dp.property("node_id", boost::get(&VertexProperty::node_id, g));
-    boost::read_graphml(inFile, g, dp);
-    return g;
-}
-
-void WriteGraph(std::ostream& os, Graph& graph) {
-    boost::dynamic_properties dp;
-    dp.property("label", boost::get(&VertexProperty::label, graph));
-    dp.property("value", boost::get(&VertexProperty::value, graph));
-    dp.property("node_id", boost::get(&VertexProperty::node_id, graph));
-    boost::write_graphml(os, graph, dp, true);
-}
 
 int main() {
-
-    Graph g = ReadGraph("../graph.graphml");
-    WriteGraph(std::cout << "==== XML version: ====\n\n", g);
+    Graph g = GraphAlgos::read_graphml("../graph.graphml");
+    GraphAlgos::write_dot(std::cout, g);
     return 0;
 }
